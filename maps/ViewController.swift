e619e79ba736e74a64390374ff1d977dd0cee761
West Kraemer <WestKraemer@gmail.com>
2017-02-15 14:10:12 -0500

    add basic map coordinates

--- maps/ViewController.swift
+++ maps/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 40.7
+        
+        let longitude: CLLocationDegrees = -73.9
+        
+        let latDelta: CLLocationDegrees = 0.05
+        
+        let longDelta: CLLocationDegrees = 0.05
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        map.setRegion(region, animated: true)        
+        
     }
 
     override func didReceiveMemoryWarning() {
