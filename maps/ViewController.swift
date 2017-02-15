8501d6df9163c5771ff820aed9a2f0d04525751d
West Kraemer <WestKraemer@gmail.com>
2017-02-15 15:51:09 -0500

    add ability to long press and set a pin.

--- maps/ViewController.swift
+++ maps/ViewController.swift
@@ -31,9 +31,44 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         let region = MKCoordinateRegion(center: coordinates, span: span)
         
-        map.setRegion(region, animated: true)        
+        map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.title = "Miami"
+        
+        annotation.subtitle = "In need of city planning..."
+        
+        annotation.coordinate = coordinates
+        
+        map.addAnnotation(annotation)
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        
+        uilpgr.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(uilpgr)
         
     }
+    
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = coordinate
+        
+        annotation.title = "New Place"
+        
+        annotation.subtitle = "Maybe I'll go here..."
+        
+        map.addAnnotation(annotation)
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
